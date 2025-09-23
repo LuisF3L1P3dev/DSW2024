00e1c5a8817aefd0b8ac40428e616448b318e36d
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-09-23 16:24:11 -0300

    feat: base da atidade

---
+++ forms_arrays_js/js/scripts.js
@@ -0,0 +1,7 @@
+const produtos = [
+  { id: 1, nome: 'Notebook Gamer', categoria: 'Eletrônicos', preco: 4500 },
+  { id: 2, nome: 'Cadeira de Escritório', categoria: 'Móveis', preco: 1200 },
+  { id: 3, nome: 'Mouse sem fio', categoria: 'Eletrônicos', preco: 150 },
+  { id: 4, nome: 'Mesa de Jantar', categoria: 'Móveis', preco: 2500 },
+  { id: 5, nome: 'Teclado Mecânico', categoria: 'Eletrônicos', preco: 450 }
+];
