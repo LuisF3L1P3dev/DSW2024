8aeee87e7ad276af73951429e60bd781ea9affb3
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-10-21 16:06:49 -0300

    feat: caminho static para imagens portfolio

--- templatesNavegacao/paginas/views.py
+++ templatesNavegacao/paginas/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 def index(request):
    return render(request, 'paginas/index.html')
 
-
 def base(request):
    return render(request, 'base.html')
+
+def portfolio(request):
+   return render(request, 'paginas/portfolio.html')
