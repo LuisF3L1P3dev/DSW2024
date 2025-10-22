0522d13a544f25cde06c7f6516f3e6082432bb4c
Luís Felipe da Silva Dantas <20221094040014@ifrn.local>
2025-10-22 16:00:34 -0300

    feat: filtragem, exibição e pesquisa do model na aba de admin, e add produtos no banco pelo admin

--- gerenciamentoProdutos/paginas/admin.py
+++ gerenciamentoProdutos/paginas/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
-admin.site.register(Produto)
+# Register your models here.
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome','codigo','preco','quantidade', 'data_criacao']
+    list_filter = ['nome', 'codigo']
+    search_fields = ['data_criacao']
+    ordering = ['-data_criacao']
+
+admin.site.register(Produto,ProdutoAdmin)
