0deb31b6ee88b44d29b2fd947cf7ed84b7a68579
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-10-22 14:29:04 -0300

    fix: max digits de produtos

--- gerenciamentoProdutos/paginas/models.py
+++ gerenciamentoProdutos/paginas/models.py
@@ -5,7 +5,7 @@ class Produto(models.Model):
   nome = models.CharField(max_length=50)
   codigo = models.CharField(max_length=50)
   descricao = models.TextField()
-  preco = models.DecimalField(decimal_places=2)
+  preco = models.DecimalField(decimal_places=2, max_digits=5)
   quantidade = models.IntegerField()
   data_criacao = models.DateTimeField(auto_now_add=True)
 
