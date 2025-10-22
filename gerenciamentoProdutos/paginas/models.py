f05155c4e1abcfe7ddf21ffe5b2d3629d1dab0f7
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-10-22 14:25:35 -0300

    feat: models produtos

--- gerenciamentoProdutos/paginas/models.py
+++ gerenciamentoProdutos/paginas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+  nome = models.CharField(max_length=50)
+  codigo = models.CharField(max_length=50)
+  descricao = models.TextField()
+  preco = models.DecimalField(decimal_places=2)
+  quantidade = models.IntegerField()
+  data_criacao = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.nome
