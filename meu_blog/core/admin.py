47266dbeda826365b1993b5ec132f770abb3cf71
Luís Felipe da Silva Dantas <20221094040014@ifrn.local>
2025-10-23 14:27:31 -0300

    feat:add models ao admin, populando banco, e filtragem de objetos

--- meu_blog/core/admin.py
+++ meu_blog/core/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Autor, Post
 # Register your models here.
 
-admin.site.register([Autor, Post])
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email']
+    search_fields = ['nome']
+
+admin.site.register(Autor, AutorAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['titulo','status','data_pub']
+    list_filter = ['status','data_pub']
+    search_fields = ['titulo']
+    ordering = ['-data_pub']
+admin.site.register(Post, PostAdmin)
